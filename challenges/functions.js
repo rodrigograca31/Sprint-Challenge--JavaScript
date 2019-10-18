670a27de70ff46d4067127d41e06ea813cb5c47c
rodrigograca31 <rodrigograca1993@gmail.com>
2019-10-18 08:13:35 +0100

    Formatting

--- challenges/functions.js
+++ challenges/functions.js
@@ -31,12 +31,12 @@
 const external = "I'm outside the function";
 
 function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+	console.log(external);
+	const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
+	function nestedFunction() {
+		console.log(internal);
+	};
+	nestedFunction();
 }
 myFunction();
